cc4e1e9614520b68ed8e196c23cd00ad3c814b6e
Jeanett Melsom <din@email.comjeanettk.m@hotmail.com>
2023-11-12 17:20:34 +0100

    added main.yml file

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "tc37ox",
+  projectId: 'tqkces',
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 });
