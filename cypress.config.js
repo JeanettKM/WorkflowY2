851fbb248bd209eb62d240dca1db6754f0e5f7c1
Jeanett Melsom <din@email.comjeanettk.m@hotmail.com>
2023-11-12 21:03:40 +0100

    added, installed Vite

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "tqkces",
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents(on, config) {},
   },
 });
