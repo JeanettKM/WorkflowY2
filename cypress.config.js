6525df2744f640fe178bd7a3c131f39dc7764980
Jeanett Melsom <din@email.comjeanettk.m@hotmail.com>
2023-11-12 17:57:55 +0100

    Test commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'tqkces',
+  projectId: "tqkces",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
