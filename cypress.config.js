52404a583b743b735b6f63cec4595d22947cff07
Jeanett Melsom <din@email.comjeanettk.m@hotmail.com>
2023-11-12 16:26:17 +0100

    trying to troubleshoot github actions issues

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "tc37ox",
   e2e: {
     setupNodeEvents(on, config) {},
   },
--- cypress/e2e/es6.test.cy.js
+++
@@ -1,9 +0,0 @@
-import { wait } from "./es6.cy.js";
-
-it("It resolves with 'abc' value after 1 second", async () => {
-  const start = Date.now();
-  const data = await wait(1000, "abc");
-  expect(data).to.equal("abc");
-  const end = Date.now();
-  expect(end - start).to.be.gte(1000);
-});
