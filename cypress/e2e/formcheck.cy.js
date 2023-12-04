c71dd1ad7815fc230820c70b0dc657e47f884070
Jeanett Melsom <jeanettkestnermelsom@hotmail.com>
2023-12-04 16:05:10 +0100

    added changes according to teachers request per PR

--- cypress/e2e/formcheck.cy.js
+++ cypress/e2e/formcheck.cy.js
@@ -1,7 +1,7 @@
 const userEmail = "jeanett.kestner@stud.noroff.no";
 const userPassword = "Kestner12";
-const incorrectEmail = "testing@noroff.no";
-const incorrectPassword = "passwordTest123";
+const incorrectEmail = "testing@hotmail.no";
+const incorrectPassword = "Test1";
 
 describe("Test for form validation and error handling", () => {
   beforeEach(() => {
@@ -11,21 +11,35 @@ describe("Test for form validation and error handling", () => {
     cy.wait(1000);
   });
 
-  it("Displays error for wrong password", () => {
-    cy.login(userEmail, incorrectPassword);
-  });
-
   it("Displays error for wrong email", () => {
     cy.login(incorrectEmail, userPassword);
+    cy.get("#loginForm").submit();
+    cy.get("#loginEmail").then(($input) => {
+      expect($input[0].validity.patternMismatch).to.be.true;
+    });
+  });
+
+  it("Displays error for wrong password", () => {
+    cy.login(userEmail, incorrectPassword);
+    cy.get("#loginForm").submit();
+    cy.get("#loginPassword").should("have.attr", "minlength", "8");
   });
 
   it("Displays error for empty input field", () => {
-    cy.login("a", "a");
+    cy.get("#loginEmail").clear();
+    cy.get("#loginPassword").clear();
+    cy.get("#loginForm button[type=submit]").click();
+    cy.get("#loginEmail").then(($input) => {
+      expect($input[0].checkValidity()).to.be.false;
+    });
+    cy.get("#loginPassword").then(($input) => {
+      expect($input[0].checkValidity()).to.be.false;
+    });
   });
 });
 
 Cypress.Commands.add("login", (email, password) => {
-  cy.get("#loginEmail").type(email).type("{enter}");
+  cy.get("#loginEmail").type(email);
   cy.get("#loginPassword").type(password);
   cy.get("#loginForm button[type=submit]").contains("Login").click();
 });
